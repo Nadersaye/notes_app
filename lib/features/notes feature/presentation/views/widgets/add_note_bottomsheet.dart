ab4f313aee8f1835eef5aad032494f1af18f3c8f
Nader <ns1568@fayoum.edu.eg>
2023-10-28 17:13:57 +0200

    create custom note textformfield border

--- lib/features/notes feature/presentation/views/widgets/add_note_bottomsheet.dart
+++ lib/features/notes feature/presentation/views/widgets/add_note_bottomsheet.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
+import 'custom_note_textformfield.dart';
+
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 24,
+          ),
+          CustomNoteTextFormfield(
+            hint: 'Title',
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ lib/features/notes feature/presentation/views/widgets/custom_note_textformfield.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustomNoteTextFormfield extends StatelessWidget {
+  const CustomNoteTextFormfield({super.key, required this.hint});
+  final String hint;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      cursorColor: const Color(0xff62fcd7),
+      decoration: InputDecoration(
+          hintText: hint,
+          focusedBorder:
+              customTextformfieldBorder(borderColor: const Color(0xff62fcd7)),
+          enabledBorder: customTextformfieldBorder(borderColor: null)),
+    );
+  }
+
+  OutlineInputBorder customTextformfieldBorder(
+          {@required Color? borderColor}) =>
+      OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(color: borderColor ?? Colors.white));
+}
