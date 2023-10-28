4ae0057c63a9b4afadeda496816541f8719676ec
Nader <ns1568@fayoum.edu.eg>
2023-10-28 15:04:38 +0200

    show add note bottom sheet

--- lib/features/notes feature/presentation/views/notes_view.dart
+++ lib/features/notes feature/presentation/views/notes_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/features/notes%20feature/presentation/views/widgets/notes_view_body.dart';
+import 'widgets/add_note_bottomsheet.dart';
+import 'widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
@@ -9,7 +10,11 @@ class NotesView extends StatelessWidget {
     return Scaffold(
       body: const NotesViewBody(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) => const AddNoteBottomSheet());
+        },
         child: const Icon(Icons.add),
       ),
     );
