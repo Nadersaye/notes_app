511c9f9959aa858bd926da98036bd98f34739b72
Nader <ns1568@fayoum.edu.eg>
2023-10-30 00:51:51 +0200

    provide note cubit

--- lib/features/notes feature/presentation/views/widgets/Add note widgets/custom_note_item.dart
+++ lib/features/notes feature/presentation/views/widgets/Add note widgets/custom_note_item.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import '../../../../data/models/note_model.dart';
 import '../../edit_note_view.dart';
 import 'add_notes_item_content.dart';
 
 class CustomNoteItem extends StatelessWidget {
-  const CustomNoteItem({super.key});
-
+  const CustomNoteItem({super.key, required this.notes});
+  final NoteModel notes;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -17,7 +18,7 @@ class CustomNoteItem extends StatelessWidget {
         padding: const EdgeInsets.fromLTRB(16, 24, 0, 24),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16),
-          color: const Color(0xffffcc80),
+          color: Color(notes.color),
         ),
         child: const AddNotesItemContent(),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/core/constants.dart';
 import 'package:notes_app/core/simple_bloc_observer.dart';
+import 'package:notes_app/features/notes%20feature/presentation/manager/notes%20cubit/notes_cubit.dart';
 import 'package:notes_app/features/notes%20feature/presentation/views/add_notes_view.dart';
 
 import 'features/notes feature/data/models/note_model.dart';
@@ -20,10 +21,17 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark, fontFamily: "Poppins"),
-      home: const AddNotesView(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) {
+          return NotesCubit();
+        })
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.dark, fontFamily: "Poppins"),
+        home: const AddNotesView(),
+      ),
     );
   }
 }
